fe0063bcd319b3dee6dd51bd1c1a6c4321c3f5e2
I-AM-CHAMI <88298450+chamiofficial@users.noreply.github.com>
2025-06-16 17:48:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=3QdEmBCD#GVewSdlrgzJIvEPMfP9pJ6__i9gzkJPnFHv4LpNSlXc' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=nZ1SEZJS#VQqN5Lqegr5B_Y4YYadrAfgHAS3RhzRoLI2eTuFa1qo' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
